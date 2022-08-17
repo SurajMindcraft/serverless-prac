7765100728764e2f28a0c5c363a702ef988a8ab4
Suraj sahu <suraj.sahu@mindcraft.in>
2022-08-17 12:49:20 +0530

    .

--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,7 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  let rand = parseInt(Math.random *100);
+  console.log(`Random number is : ${rand}`);
+  return rand;
 };
